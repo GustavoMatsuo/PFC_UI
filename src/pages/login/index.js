8eb2603cc6193f124804a34964d46431f3313d42
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-06-16 16:05:26 -0300

    chore: improvement login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,35 +1,63 @@
 import { styled } from '@mui/material/styles'
-import { Container, Typography } from '@mui/material'
+import { Container, Typography, Card } from '@mui/material'
 import { LoginForm } from './LoginForm'
+import useResponsive from '../../hooks/useResponsive'
+import './AnimatedBackground.css'
 
 const RootStyle = styled('div')(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex',
-  },
+  display: 'flex',
+  height: '100vh',
+  width: '100vw',
+  flexDirection: 'row',
+  justifyContent: 'space-around'
 }))
 
 const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  minHeight: '100vh',
+  width: 480,
+  height: '100%',
   display: 'flex',
-  justifyContent: 'center',
   flexDirection: 'column',
-  padding: theme.spacing(12, 0),
+  justifyContent: 'center',
 }))
 
+const SectionStyle = styled('div')(({ theme }) => ({
+  width: '100%',
+  maxWidth: 464,
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+}));
+
 export default function Login() {
+  const mdUp = useResponsive('up', 'md')
+
   return (
-    <RootStyle>
-      <Container maxWidth="sm">
+    <>
+      <RootStyle>
+        {mdUp? (
+          <SectionStyle>
+            <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
+              Olá! Seja bem-vindo.
+            </Typography>
+            {/* <img src="/static/illustrations/illustration_login.png" alt="login" /> */}
+          </SectionStyle>
+        ): (<span/>)}
         <ContentStyle>
-          <Typography variant="h4" gutterBottom>
-            Entrar
-          </Typography>
-          <Typography sx={{ color: 'text.secondary', mb: 3 }}>Coloque seus dados abaixo</Typography>
-          <LoginForm />
+          <Card sx={{ padding: 3 }}>
+            <Typography variant="h4" gutterBottom>
+              Entrar
+            </Typography>
+            <Typography sx={{ color: 'text.secondary', mb: 3 }}>Coloque seus dados abaixo</Typography>
+            <LoginForm />
+          </Card>
         </ContentStyle>
-      </Container>
-    </RootStyle>
+        {!mdUp && (<span/>)}
+      </RootStyle>
+      <div class='box'>
+        <div class='wave -one'></div>
+        <div class='wave -two'></div>
+        <div class='wave -three'></div>
+      </div>
+    </>
   )
 }
