331fe7a8ab08de08ca62e79c287511ff757341a5
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-06-16 22:38:30 -0300

    chore add locale date-fns

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,19 +1,21 @@
 import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
 export function fDate(date) {
-  return format(new Date(date), 'dd MMMM yyyy')
+  return format(new Date(date), 'dd MMMM yyyy', { locale: ptBR })
 }
 
 export function fDateTime(date) {
-  return format(new Date(date), 'dd MMM yyyy HH:mm')
+  return format(new Date(date), 'dd MMM yyyy HH:mm', { locale: ptBR })
 }
 
 export function fDateTimeSuffix(date) {
-  return format(new Date(date), 'dd/MM/yyyy hh:mm p')
+  return format(new Date(date), 'dd/MM/yyyy hh:mm p', { locale: ptBR })
 }
 
 export function fToNow(date) {
   return formatDistanceToNow(new Date(date), {
     addSuffix: true,
+    locale: ptBR
   })
 }
