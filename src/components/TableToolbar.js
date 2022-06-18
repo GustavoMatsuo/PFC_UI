ff08190f90bff73aab683a9b237c78eacb900cf3
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-06-18 13:08:59 -0300

    chore: add TableToolbar in Components

---
+++ src/components/TableToolbar.js
@@ -0,0 +1,52 @@
+import PropTypes from 'prop-types'
+import { styled } from '@mui/material/styles'
+import { Toolbar, OutlinedInput, InputAdornment } from '@mui/material'
+import Iconify from './Iconify'
+
+const RootStyle = styled(Toolbar)(({ theme }) => ({
+  height: 96,
+  display: 'flex',
+  justifyContent: 'space-between',
+  padding: theme.spacing(0, 1, 0, 3),
+}))
+
+const SearchStyle = styled(OutlinedInput)(({ theme }) => ({
+  width: 240,
+  transition: theme.transitions.create(['box-shadow', 'width'], {
+    easing: theme.transitions.easing.easeInOut,
+    duration: theme.transitions.duration.shorter,
+  }),
+  '&.Mui-focused': { width: 320, boxShadow: theme.customShadows.z8 },
+  '& fieldset': {
+    borderWidth: `1px !important`,
+    borderColor: `${theme.palette.grey[500_32]} !important`,
+  },
+}))
+
+TableToolbar.propTypes = {
+  placeholder: PropTypes.string,
+  filterName: PropTypes.string,
+  onFilterName: PropTypes.func,
+}
+
+export function TableToolbar({ placeholder, filterName, onFilterName }) {
+  return (
+    <RootStyle>
+      <SearchStyle
+        value={filterName}
+        onChange={onFilterName}
+        placeholder={placeholder}
+        startAdornment={
+          <InputAdornment position="start">
+            <Iconify icon="eva:search-fill" sx={{ color: 'text.disabled', width: 20, height: 20 }} />
+          </InputAdornment>
+        }
+      />
+      {/* <Tooltip title="Filter list">
+        <IconButton>
+          <Iconify icon="ic:round-filter-list" />
+        </IconButton>
+      </Tooltip> */}
+    </RootStyle>
+  )
+}
