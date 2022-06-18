bc1e04fb522a49b43170ef582cedc8351360c244
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-06-18 13:07:56 -0300

    chore: add BaseOptionChart

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { BaseOptionChartStyle } from './components/chart/BaseOptionChart'
 import Router from './routes'
 import ThemeProvider from './theme'
 
 export default function App() {
   return (
     <ThemeProvider>
+      <BaseOptionChartStyle />
       <Router />
     </ThemeProvider>
   )
