3241401f2ecb1c24ced495fa81c53ca1976b9af6
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-10-17 20:35:48 -0300

    chore: remove chart on Entrada

--- src/pages/Entrada/index.js
+++ src/pages/Entrada/index.js
@@ -224,18 +224,6 @@ export default function Entrada() {
 
   return (
     <div>
-      <Grid container spacing={3} sx={{mb: 3}}>
-        <Grid item xs={12} sm={12} md={4}>
-          <CardProduto title="Subtitulo"/>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <CardProduto title="Subtitulo"/>
-        </Grid>
-        <Grid item xs={12} sm={12} md={4}>
-          <CardProduto title="Subtitulo"/>
-        </Grid>
-      </Grid>
-
       <Card>
         <TableToolbar 
           placeholder='Pesquisar Entrada...' 
