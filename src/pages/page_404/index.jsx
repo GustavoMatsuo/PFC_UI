82d343d13d20885dece402c68a943e63ede589b5
Gustavo Matsuo <GustavoMatsuoF@gmail.com>
2022-10-29 20:04:31 -0300

    chore: improve 404 page

--- src/pages/page_404/index.jsx
+++ src/pages/page_404/index.jsx
@@ -1,31 +1,42 @@
 import { Link as RouterLink } from 'react-router-dom'
 import { styled } from '@mui/material/styles'
 import { Button, Typography, Container } from '@mui/material'
+import IMG_MOCK from '../../assets/images/crash-amico.svg'
 
 const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
   margin: 'auto',
   minHeight: '100vh',
   display: 'flex',
   justifyContent: 'center',
   flexDirection: 'column',
-  padding: theme.spacing(12, 0)
+  alignItems: 'center',
+  padding: theme.spacing(12, 0),
+  textAlign: 'center', 
 }))
 
 export default function Page404() {
   return (
     <Container>
-      <ContentStyle sx={{ textAlign: 'center', alignItems: 'center' }}>
+      <ContentStyle>
+        <div 
+          style={{
+            height: '50vh',
+            width: '100%',
+            backgroundImage: `url(${IMG_MOCK})`,
+            backgroundPosition: 'center',
+            backgroundRepeat: 'no-repeat',
+          }}
+        />
         <Typography variant="h3" paragraph>
           Ops! Página não encontrada.
         </Typography>
 
-        <Typography sx={{ color: 'text.secondary' }} mb={5}>
+        <Typography sx={{ color: 'text.secondary' }} mb={3}>
           Desculpe, mas não conseguimos encontrar a página que você está procurando.
         </Typography>
 
         <Button to="/dashboard/home" size="large" variant="contained" component={RouterLink}>
-          Voltar
+          Voltar para home
         </Button>
       </ContentStyle>
     </Container>
